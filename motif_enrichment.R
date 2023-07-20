ecdf1b7773c763e45eea122dae2b9848717c8f2b
anas fathul <muhdanas@uw.edu>
2023-07-19 20:13:11 -0700

    Add new columns ncomponents

--- motif_enrichment.R
+++ motif_enrichment.R
@@ -37,18 +37,21 @@ motif_indicator <- read.table(args[2])
 motif_count_sum <- sum(motif_indicator)
 motif_indicator <- unlist(motif_indicator)
 
-prefix_name <- args[3]
+motif_id <- args[3]
+n_components <- args[4]
 
 print("Running logistic regression model")
 log_model = glm(motif_indicator ~., data=DHS_feature_nmf_df, family=binomial(link="logit"))
 
 # Set up dataframe for coefficents and rename column
+
 coef_df <- as.data.frame(summary(log_model)['coefficients'])
-coef_df['motif_id'] <- prefix_name
+coef_df['motif_id'] <- motif_id
 names(coef_df)[1] <- "estimate"
 names(coef_df)[2] <- "std_error"
 names(coef_df)[3] <- "z_value"
 names(coef_df)[4] <- "Pr(>|z|)"
+coef_df['ncomponents'] <- n_components
 
 # Calculate AUC & PR
 pred_probs = predict(log_model, type = "response")
@@ -56,17 +59,22 @@ roc_score <- roc.curve(scores.class0 = pred_probs, weights.class0 = motif_indica
 pr_score <- pr.curve(scores.class0 = pred_probs, weights.class0 = motif_indicator, curve = TRUE)
 
 # save the list
-roc_pr <- list(roc_auc = roc_score$auc, pr_auc_integral = pr_score$auc.integral,
- pr_auc_davis = pr_score$auc.davis.goadrich, motif_count = motif_count_sum, motif_id = prefix_name)
+roc_pr <- list(
+        roc_auc = roc_score$auc, 
+        pr_auc_integral = pr_score$auc.integral,
+        pr_auc_davis = pr_score$auc.davis.goadrich,
+        motif_count = motif_count_sum, 
+        motif_id = prefix_name
+)
 
 roc_pr_df <- as.data.frame(roc_pr)
 
 # Save in .csv format
 print("Writing roc_pr csv file")
-csv_name1 <- paste(prefix_name, ".metrics.tsv", sep='')
+csv_name1 <- paste(motif_id, '.', n_components, ".metrics.tsv", sep='')
 write.table(roc_pr_df, file=csv_name1, quote=FALSE, sep='\t')
 
 # Save in .csv format X1, X2, ..., X16, PRROC score, PRAUC score
-csv_name2 <- paste(prefix_name, ".coeff.tsv", sep='')
+csv_name2 <- paste(motif_id, '.', n_components, ".coeff.tsv", sep='')
 print("Writing coefficient csv file")
 write.table(coef_df, file=csv_name2, quote=FALSE, sep='\t')
